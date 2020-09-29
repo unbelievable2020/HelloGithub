3326e49da3ded2ae4f852a5b938183cc80d376f2
xzq <xzq2020@stu.pku.edu.cn>
2020-09-29 19:08:53 +0800

    test

--- src/com/xzq/Main.java
+++ src/com/xzq/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("111王八蛋黄鹤老板!\n");
+        System.out.println("112221王八蛋黄鹤老板!\n");
     }
 }
